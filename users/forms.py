3ffa1b5b68df618740bef28d83e7276ed635c83e
Vladimir <vova.derkach.89@mail.ru>
2023-08-27 22:29:05 +0300

    commit work home 27/08/2023

---
+++ users/forms.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User
+from django.utils.translation import gettex as _
+from django import forms
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+        help_texts = {
+            'username': 'Только буквы, цифры и символы @/./+/-/_',
+        }
+        verbose_name = {
+            'username': 'login',
+        }
+
+
+        error_messages = {
+            'username': {
+                'required': 'Это поле обязательно для заполнения',
+                 },
+            
+            'password': {
+                'required': 'Это поле обязательно для заполнения',
+            },
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return cd['password2']
